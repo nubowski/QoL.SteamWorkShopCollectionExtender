8b123fcf0ee6c4f614c3803a2725d4e55334d640
nubowski <nubowski@nnteam.ru>
2023-03-25 01:16:12 +0300

    feat: working script with some warns

--- WorkShopExtender.js
+++ WorkShopExtender.js
@@ -29,3 +29,39 @@ function addToCollection(data, object) {
     });
 }
 
+setTimeout(function () {
+    const collectionWindow = document.querySelector('div.collectionAddItemsSection');
+    const addButton = createButton('ASCM_addall', '✓', 60);
+    const removeButton = createButton('ASCM_removeall', '⚠', 150);
+
+    addButton.style.background = '#002f06';
+    removeButton.style.background = '#7e1a1a';
+
+    collectionWindow.insertBefore(addButton, collectionWindow.firstChild);
+    collectionWindow.insertBefore(removeButton, collectionWindow.firstChild);
+
+    // Bind "Add" button
+    jQuery('button#ASCM_addall').click(function () {
+        const items = [];
+        const collectionName = jQuery('div.manageCollectionHeader div.breadcrumbs a').eq(2).text().trim();
+        const url = new URL(document.location.href);
+        const collectionId = url.searchParams.get('id');
+
+        jQuery('div#MySubscribedItems div.itemChoice:not(.inCollection)').each(function () {
+            const data = {
+                id: collectionId,
+                sessionid: window.g_sessionID,
+                childid: jQuery(this).attr('id').replace('choice_MySubscribedItems_', ''),
+                activeSection: collectionName,
+            };
+            addToCollection(data, jQuery(this));
+        });
+    });
+
+    // Bind "Remove" button
+    jQuery('button#ASCM_removeall').click(function () {
+        jQuery('div#MySubscribedItems div.itemChoice.inCollection').each(function () {
+            window.RemoveChildFromCollection(jQuery(this).attr('id').replace('choice_MySubscribedItems_', ''));
+        });
+    });
+}, 0);
